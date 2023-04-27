40cb41b76ec53f1153ebee138e89e0834b50ddaa
Oleg Degay <32535663+odegay@users.noreply.github.com>
2023-04-27 15:02:01 -0400

    Create helper2.js

---
+++ helper2.js
@@ -0,0 +1,10 @@
+import * as http from 'http';
+import * as url from 'url';
+
+function test1 (x: int) {
+  if (x == 0) {
+    console.log('x = 0');
+  } else if (x == 1) {
+    console.log('x = 1');
+  }
+}
