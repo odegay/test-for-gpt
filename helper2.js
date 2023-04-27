71bceb3bf1d204913c1297c0e35a2f89dcc9cdb4
Oleg Degay <32535663+odegay@users.noreply.github.com>
2023-04-27 16:17:36 -0400

    Update helper2.js

--- helper2.js
+++ helper2.js
@@ -8,3 +8,11 @@ function test1 (x: int) {
     console.log('x = 1');
   }
 }
+
+//function test1 (x: int) {
+//  if (x == 0) {
+//    console.log('x = 0');
+//  } else if (x == 1) {
+//    console.log('x = 1');
+//  }
+//}
