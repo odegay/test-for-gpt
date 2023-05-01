acfe9435f721ae0729d9a470420f9a03ec6f8cd0
robot <robot@noemail.com>
2023-05-01 06:23:29 +0000

    -=TEST SINGLE RUN=- Apply automated fixes

--- scripts/helper2.js
+++ scripts/helper2.js
@@ -2,15 +2,15 @@ import * as http from 'http';
 import * as url from 'url';
 
 function test1 (x: int) {
-  if (x == 0) {
+  if (x === 0) {
     console.log('x = 0');
-  } else if (x == 1) {
+  } else if (x === 1) {
     console.log('x = 1');
   }
 }
 
 function test2(path: string) {
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, ''); 
+  const parsedUrl = url.parse(path, true);
+  const trimmedPath = path.replace(/(^\/+|\/+$)/g, ''); 
   return trimmedPath;
 }
