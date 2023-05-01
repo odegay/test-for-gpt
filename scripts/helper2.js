45db497fc5c2485479ffa234cb1698867de41252
robot <robot@noemail.com>
2023-05-01 05:05:35 +0000

    -=TEST SINGLE RUN=- Apply automated fixes

--- scripts/helper2.js
+++ scripts/helper2.js
@@ -1,16 +1,15 @@
 import * as http from 'http';
-import * as url from 'url';
 
-function test1 (x: int) {
-  if (x == 0) {
+function test1 (x: number) {
+  if (x === 0) {
     console.log('x = 0');
-  } else if (x == 1) {
+  } else if (x === 1) {
     console.log('x = 1');
   }
 }
 
-function test2(path: string) {
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, ''); 
+function test2(urlPath: string) {
+  const path = url.parse(urlPath).pathname;
+  const trimmedPath = path.replace(/(^\/+|\/+$)/g, ''); 
   return trimmedPath;
 }
