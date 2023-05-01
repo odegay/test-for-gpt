0619e6b12b65eb5b8bcfc4bcec17015d7aabda46
robot <robot@noemail.com>
2023-05-01 01:44:54 +0000

    Apply automated fixes

--- scripts/helper2.js
+++ scripts/helper2.js
@@ -1,15 +1,15 @@
 import * as http from 'http';
-import * as url from 'url';
 
-function test1 (x: int) {
-  if (x == 0) {
+function test1 (x: number) {
+  if (x === 0) {
     console.log('x = 0');
-  } else if (x == 1) {
+  } else if (x === 1) {
     console.log('x = 1');
   }
 }
 
-function test2(path: string) {
+function test2(url: string) {
+  const parsedUrl = new URL(url);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, ''); 
   return trimmedPath;
