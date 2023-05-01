6d13a72467ea4b0e673330f1e0102bff916d3a66
robot <robot@noemail.com>
2023-05-01 17:51:00 +0000

    Apply automated fixes

--- scripts/helper2.js
+++ scripts/helper2.js
@@ -1,15 +1,18 @@
-import * as http from 'http';
+Updated code with the requested changes is shown below.
 
-function test1 (x: int) {
-  if (x == 0) {
-    console.log('x = 0');
-  } else if (x == 1) {
-    console.log('x = 1');
-  }
+```
+function test1 (x: number) {
+   if (x === 0) {
+     console.log('x = 0');
+   } else if (x === 1) {
+     console.log('x = 1');
+   }
 }
 
 function test2(urlPath: string) {
-  const path = url.parse(urlPath).pathname;
+  const path = new URL(urlPath).pathname;
   const trimmedPath = path.replace(/(^\/+)|(\/+$)/g, ''); 
   return trimmedPath;
 }
+```
+Note: I have made additional changes to the code to fix other issues I found while reviewing the code.
